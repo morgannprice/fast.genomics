18488f8d4e2f1773d32b93366aff357327d92ced
Morgan Price <funwithwords26@gmail.com>
2023-01-19 16:47:17 -0800

    Add neighbor::readMMSeqsHits

--- lib/neighbor.pm
+++ lib/neighbor.pm
@@ -5,7 +5,7 @@ use strict;
 
 our (@ISA,@EXPORT);
 @ISA = qw(Exporter);
-@EXPORT = qw!parseTaxString parseLast!;
+@EXPORT = qw!parseTaxString parseLast readMMSeqsHits!;
 
 # Returns a hash of d/p/c/o/f/g/s to value
 # (any of which could be missing),
@@ -104,6 +104,39 @@ sub parseLast($) {
     }
   }
   die "Ended MAF file without finishing alignment" if defined $scoreLine;
+  return \@out;
+}
+
+# Given a file with mmseqs hits, returns a reference to a list of
+# hashes, each with query, subject, identity (as a fraction, not a
+# percentage), alnLength, nMismatch, nGapOpens, qBegin, qEnd, sBegin,
+# sEnd, eValue, bits
+sub readMMSeqsHits($) {
+  my ($file) = @_;
+  open (my $fh, "<", $file) || die "Cannot read $file";
+  my @colNames = qw{query subject identity alnLength nMismatch nGapOpens qBegin qEnd sBegin sEnd eValue bits};
+  my @out;
+  while (my $line = <$fh>) {
+    chomp $line;
+    my @F = split /\t/, $line;
+    die "Wrong number of columns in mmseqs hits file $file"
+      unless scalar(@F) == scalar(@colNames);
+    my %row = ();
+    foreach my $i (0..(scalar(@F)-1)) {
+      die "Empty column in mmseqs hits file $file" unless $F[$i] =~ m/\S/;
+      $row{ $colNames[$i] } = $F[$i];
+    }
+    foreach my $field (qw{alnLength nMismatch nGapOpens qBegin qEnd sBegin sEnd}) {
+      die "Invalid $field in mmseqs hits file $file"
+        unless $row{$field} =~ m/^\d+$/;
+    }
+    die "Invalid bits in mmseqs file $file"
+      unless $row{bits} =~ m/^[0-9.]+$/;
+    die "Invalid E-value in mmseqs file $file"
+      unless $row{eValue} =~ m/^[0-9.E+-]+$/;
+    push @out, \%row;
+  }
+  close($fh) || die "Error reading $file";
   return \@out;
 }
 
