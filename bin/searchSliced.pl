6ac36f74fc85fafcfb9ad4921e5522c3bf00d7b6
Morgan Price <funwithwords26@gmail.com>
2023-05-30 09:26:30 -0700

    searchSliced.pl: Switch to max-seqs = 10*limit by default

--- bin/searchSliced.pl
+++ bin/searchSliced.pl
@@ -6,7 +6,8 @@ use FindBin qw{$RealBin};
 my $nThreads = 1; # 1 thread per slice
 # These defaults are as in mmseqs itself
 my $eValue = 0.001;
-my $maxSeqs = 300;
+my $limit = 300;
+my $nAlignFactor = 10;
 my $dbLoadMode = 1;
 my $sens = 5.7;
 
@@ -16,34 +17,37 @@ Runs mmseqs easy-search on each slice (prefix0, prefix1, ...)
 and combines the results.
 
 Optional arguments:
--e $eValue
 -s $sens -- controls the sensitivity of mmseqs2
--max-seqs $maxSeqs -- controls the pre-filter of mmseqs2,
-  and also limits the maximum #sequences returned
--limit -- specify the number of hits to return
-  (defaults to max-seqs)
--threads 1 -- #threads for each slice
-  (each slice is run in parallel)
+-e $eValue -- minimum evalue for results to report.
+   (Each slice is run at E <= eValue / nSlices, and then those e values
+    are then multiplied by the number of slices, and the hit is
+    discarded if the corrected e value is above the threshold)
+-limit $limit -- maximum number of hits to return
+-max-align -- the maximum #alignments to consider per query
+  defaults to limit * $nAlignFactor
+  (Each slice is run with a limit of max-align / nSlices)
+-threads $nThreads -- #threads for each slice
+  (the slices are run in parallel)
 -db-load-mode $dbLoadMode -- use 2 if you ran touchSliced.pl
 END
 ;
 
-my ($inFaa, $pre, $outFile, $limit, $debug);
+my ($inFaa, $pre, $outFile, $maxAlign, $debug);
 die $usage
   unless GetOptions('in=s' => \$inFaa, 'sliced=s' => \$pre, 'out=s' => \$outFile,
                     'debug' => \$debug,
                     's=f' => \$sens,
                     'e=f' => \$eValue,
-                    'max-seqs=i' => \$maxSeqs,
                     'limit=i' => \$limit,
+                    'max-align=i' => \$maxAlign,
                     'threads=i' => \$nThreads,
                     'db-load-mode=i' => \$dbLoadMode)
   && defined $inFaa && defined $pre;
 die "#threads per slice must be in 1 to 100\n" unless $nThreads >= 1 && $nThreads <= 100;
 die "e value must be positive\n" unless $eValue > 0;
-die "max-seqs must be positive\n" unless $maxSeqs > 0;
-$limit = $maxSeqs if !defined $limit;
 die "limit must be positive\n" unless $limit > 0;
+$maxAlign = $nAlignFactor * $limit if !defined $maxAlign;
+die "max-align must be >= limit\n" unless $maxAlign >= $limit;
 
 die "No such file: $inFaa\n" unless -e $inFaa;
 
@@ -63,14 +67,14 @@ die "Not a directory: $tmp\n" unless -d $tmp;
 my $tmpDir = "$tmp/buildSliced.$$";
 mkdir($tmpDir) || die "Cannot mkdir $tmpDir";
 
+my $maxAlignSlice = int(0.5 + $maxAlign  / $nSlices);
+$maxAlignSlice = 1 if $maxAlignSlice < 1;
 my $eValueSlice = $eValue / $nSlices;
-my $maxSeqsSlice = int(0.5 + 1.1 * $maxSeqs / $nSlices);
-$maxSeqsSlice = 1 if $maxSeqsSlice < 1;
 for (my $i = 0; $i < $nSlices; $i++) {
   # Each job needs its own temporary directory
   my $tmpSub = "$tmpDir/$i";
   mkdir($tmpSub) || die "Cannot mkdir $tmpSub\n";
-  my $cmd = "$mmseqs easy-search $inFaa $pre$i $tmpDir/hits$i $tmpSub --db-load-mode $dbLoadMode -e $eValueSlice -s $sens --max-seqs $maxSeqs --threads $nThreads > $tmpDir/log$i";
+  my $cmd = "$mmseqs easy-search $inFaa $pre$i $tmpDir/hits$i $tmpSub -e $eValueSlice --db-load-mode $dbLoadMode -s $sens --max-seqs $maxAlignSlice --threads $nThreads > $tmpDir/log$i";
   print STDERR "Running: $cmd\n" if defined $debug;
   if (my $pid = fork()) {
     ; # do nothing in the parent process
@@ -107,8 +111,6 @@ for (my $i = 0; $i < $nSlices; $i++) {
   while (my $line = <$fhHits>) {
     chomp $line;
     my @F = split /\t/, $line;
-    # adjust evalues
-    $F[10] = sprintf("%.4g", $nSlices * $F[10]);
     push @hits, \@F;
   }
   close($fhHits) || die "Error reading $tmpDir/hits$i\n";
@@ -117,13 +119,15 @@ print STDERR "Read all hits\n" if defined $debug;
 
 # sort by query (index 0) and then by bits (index 11) descending
 @hits = sort { $a->[0] cmp $b->[0] || $b->[11] <=> $a->[11] } @hits;
-# Reduce length to maxSeqs
 my %nShown = (); # query to #hits shown so far
 foreach my $row (@hits) {
   my $query = $row->[0];
+  # Reduce #hits to limit when printing
   if (!exists $nShown{$query} || $nShown{$query} < $limit) {
     $nShown{$query}++;
-    print $fhOut join("\t",  @$row) . "\n";
+    # adjust eValue and check it
+    $row->[10] = sprintf("%.4g", $nSlices * $row->[10]);
+    print $fhOut join("\t",  @$row) . "\n" if $row->[10] <= $eValue;
   }
 }
 close($fhOut) || die "Error writing to $outFile\n";
