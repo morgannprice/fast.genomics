1fe958dff48c7f10f083e9a2c6ca7f3566a4ee8e
Morgan Price <funwithwords26@gmail.com>
2024-05-24 10:38:28 -0700

    bestHitUniprot.pm uses both swiss and AlphaFold dbs

--- lib/bestHitUniprot.pm
+++ lib/bestHitUniprot.pm
@@ -7,6 +7,7 @@ package bestHitUniprot;
 require Exporter;
 use strict;
 use LWP::Simple qw{get};
+use HTML::Entities qw{encode_entities};
 our (@ISA,@EXPORT);
 @ISA = qw(Exporter);
 @EXPORT = qw{bestHitUniprot};
@@ -27,35 +28,59 @@ sub bestHitUniprot($) {
   die "Invalid input to bestHitUniprot" unless $seq =~ m/^[A-Z]+$/;
   # H  =1 means only the best hit
   my $base = "http://ekhidna2.biocenter.helsinki.fi/cgi-bin/sans/sans.cgi";
-  my $URL = "${base}?db=uniprot&mode=table&H=1&seq=$seq";
-  my $result = get($URL);
-  return $parseError unless $result;
-  my @lines = split /\n/, $result;
-  my $iHeader;
-  for (my $i = 0; $i < scalar(@lines); $i++) {
-    if ($lines[$i] =~ m!^<TR><TH>Rank.*</TR>$!i) {
-      $iHeader = $i;
-      last;
+  my @hits = ();
+
+  # SANSparallel is currently not supporting uniprot db searches (it points at swissprot instead).
+  # So, use both that and the AlphaFold database, which has some random diverse bacteria in it
+
+  foreach my $db ("swiss", "AlphaFold") {
+    my $URL = "${base}?db=${db}&mode=table&H=1&seq=$seq";
+    print "<!-- SANSparallel URL $URL -->\n";
+    my $result = get($URL);
+    return $parseError unless $result;
+    my @lines = split /\n/, $result;
+    my $iHeader;
+    for (my $i = 0; $i < scalar(@lines); $i++) {
+      if ($lines[$i] =~ m!^<TR><TH>Rank.*</TR>$!i) {
+        $iHeader = $i;
+        last;
+      }
     }
-  }
-  return { 'error' => 'cannot parse response from SANSparallel' } unless
-    defined $iHeader;
-  my $row = $lines[$iHeader+1];
+    return { 'error' => 'cannot parse response from SANSparallel' } unless
+      defined $iHeader;
+    my $row = $lines[$iHeader+1];
+    next unless $row =~ m!<TR><TD>1</TD><TD>(.*)</TD>!; # no hits
+
+    # was getting geneName and species as well from the split
+    my (undef, undef, $identity, $ranges, $alnLength, $bits, $eValue, $idSpec, $desc,
+        $species, $geneName) = split "</TD>\s*<TD[^>]*>", $row;
+    return $parseError
+      unless $ranges =~ m/^(\d+)-(\d+):(\d+)-(\d+)$/;
+    my ($qBegin, $qEnd, $sBegin, $sEnd) = ($1,$2,$3,$4);
+    my ($prefix, $uniprotId);
+    if ($db eq "swiss") {
+      $idSpec = $1 if $idSpec =~ m!^<A HREF=.*>(.*)</A>$!i;
+      return $parseError unless $idSpec =~ m/^([a-z]+)[|]([^|]+)/;
+      ($prefix, $uniprotId) = ($1,$2);
+      $species =~ s/ *$//;
+    } else { # AlphaFold
+      $desc =~ m/AFDB:AF-([^-]+)-[A-Z][0-9]+ (.*)/ || return $parseError;
+      $uniprotId = $1;
+      $desc = $2;
+      $prefix = "tr"; # a hack
+      $geneName = $idSpec;
+      $species = "";
+    }
+    my $hit = { 'uniprotId' => $uniprotId, 'geneName' => $geneName, 'species' => $species,
+                'prefix' => $prefix, desc => $desc,
+                'identity' => $identity, 'eValue' => $eValue, 'bits' => $bits,
+                'qBegin' => $qBegin, 'qEnd' => $qEnd, 'sBegin' => $sBegin, 'sEnd' => $sEnd,
+                'service' => 'SANSparallel', 'serviceURL' => $base };
+    return $hit if $identity >= 0.99;
+    push @hits, $hit;
+  } # end loop over db
   return { 'service' => 'SANSparallel', 'serviceURL' => $base}
-    unless $row =~ m!<TR><TD>1</TD><TD>(.*)</TD></TR>!; # no hits
-  my (undef, $identity, $ranges, $alnLength, $bits, $eValue, $idSpec, $desc, $species, $geneName) = split "</TD><TD>", $1;
-  $species =~ s/ *$//;
-  return $parseError unless defined $geneName;
-  $idSpec = $1 if $idSpec =~ m!^<A HREF=.*>(.*)</A>$!i;
-  return $parseError unless $idSpec =~ m/([a-z]+)[|]([^|]+)/;
-  my ($prefix, $uniprotId) = ($1,$2);
-  # parse ranges
-  return $parseError
-    unless $ranges =~ m/^(\d+)-(\d+):(\d+)-(\d+)$/;
-  my ($qBegin, $qEnd, $sBegin, $sEnd) = ($1,$2,$3,$4);
-  return { 'uniprotId' => $uniprotId, 'geneName' => $geneName, 'species' => $species,
-           'prefix' => $prefix, desc => $desc,
-           'identity' => $identity, 'eValue' => $eValue, 'bits' => $bits,
-           'qBegin' => $qBegin, 'qEnd' => $qEnd, 'sBegin' => $sBegin, 'sEnd' => $sEnd,
-           'service' => 'SANSparallel', 'serviceURL' => $base };
+    if @hits == 0;
+  @hits = sort { $b->{identity} <=> $a->{identity} } @hits;
+  return $hits[0];
 }
